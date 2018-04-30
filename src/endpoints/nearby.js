e731dac877a23ac70ad26813bccb6fe2881d4982
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-04-30 12:30:31 -0400

    Remove nearby endpoint.

--- src/endpoints/nearby.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports.nearBeaconCollectionHandler = (req, res, next) => {
-    next();
-};
-
-module.exports.nearMuseumCollectionHandler = (req, res, next) => {
-    next();
-};
